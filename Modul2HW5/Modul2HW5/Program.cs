32c8ffa8dbc05b4faed313d3ecd10feddcefeb22
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-07-17 19:33:54 +0300

    Настроить проект -Добавил StyleCop, gitignore

---
+++ Modul2HW5/Modul2HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul2HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
